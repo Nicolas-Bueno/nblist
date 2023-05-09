8f0890139668533c29ceb7a34f1742f1b1bbf632
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-05-09 18:58:27 -0300

    Project created

---
+++ src/test/java/com/nicolas/nblist/NblistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.nblist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NblistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
